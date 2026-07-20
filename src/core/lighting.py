935aa6365de13b59f23827b252995a4a41c8b0e0
okushnarev <styx.inc4@gmail.com>
2026-07-20 12:17:57 +0700

    Add local lighting class

--- src/core/lighting.py
+++ src/core/lighting.py
@@ -15,6 +15,26 @@ class AmbientLighting(BaseLighting):
         self.sun.blender_obj.data.angle = np.deg2rad(90)
         self.sun.set_energy(10)
 
+class LocalFactoryLighting(BaseLighting):
+    """The dual area light setup with ambient sunlight and factory like lamps"""
+    def setup(self) -> None:
+        # Create local lights
+        self.light_top = bproc.types.Light('AREA', 'light_top')
+        self.light_top.blender_obj.data.shape = 'RECTANGLE'
+        self.light_top.blender_obj.data.size = 0.6
+        self.light_top.blender_obj.data.size_y = 0.3
+        self.light_top.set_location([0, 0.7, 1.4])
+        self.light_top.set_rotation_euler([np.deg2rad(-45), 0, 0])
+        self.light_top.set_energy(10)
+
+        self.light_bot = bproc.types.Light('AREA', 'light_bot')
+        self.light_bot.blender_obj.data.shape = 'RECTANGLE'
+        self.light_bot.blender_obj.data.size = 0.6
+        self.light_bot.blender_obj.data.size_y = 0.3
+        self.light_bot.set_location([0, -0.7, 1.4])
+        self.light_bot.set_rotation_euler([np.deg2rad(45), 0, 0])
+        self.light_bot.set_energy(10)
+
 class ConveyorLighting(AmbientLighting):
     """The dual area light setup with ambient sunlight and factory like lamps"""
     def setup(self) -> None:
