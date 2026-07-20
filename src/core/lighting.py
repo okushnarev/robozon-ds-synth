a1ecd3d047368813854220b38de54534f6e677bb
okushnarev <styx.inc4@gmail.com>
2026-07-20 12:13:33 +0700

    Update light sources to be part of the class

--- src/core/lighting.py
+++ src/core/lighting.py
@@ -10,28 +10,28 @@ class AmbientLighting(BaseLighting):
     """The dual area light setup with ambient sunlight and factory like lamps"""
     def setup(self) -> None:
         # Create ambient light
-        sun = bproc.types.Light('SUN')
-        sun.blender_obj.data.use_shadow = False
-        sun.blender_obj.data.angle = np.deg2rad(90)
-        sun.set_energy(10)
+        self.sun = bproc.types.Light('SUN')
+        self.sun.blender_obj.data.use_shadow = False
+        self.sun.blender_obj.data.angle = np.deg2rad(90)
+        self.sun.set_energy(10)
 
 class ConveyorLighting(AmbientLighting):
     """The dual area light setup with ambient sunlight and factory like lamps"""
     def setup(self) -> None:
         super().setup()
         # Create local lights
-        light_top = bproc.types.Light('AREA', 'light_top')
-        light_top.blender_obj.data.shape = 'RECTANGLE'
-        light_top.blender_obj.data.size = 0.6
-        light_top.blender_obj.data.size_y = 0.3
-        light_top.set_location([0, 0.7, 1.4])
-        light_top.set_rotation_euler([np.deg2rad(-45), 0, 0])
-        light_top.set_energy(10)
+        self.light_top = bproc.types.Light('AREA', 'light_top')
+        self.light_top.blender_obj.data.shape = 'RECTANGLE'
+        self.light_top.blender_obj.data.size = 0.6
+        self.light_top.blender_obj.data.size_y = 0.3
+        self.light_top.set_location([0, 0.7, 1.4])
+        self.light_top.set_rotation_euler([np.deg2rad(-45), 0, 0])
+        self.light_top.set_energy(10)
 
-        light_bot = bproc.types.Light('AREA', 'light_bot')
-        light_bot.blender_obj.data.shape = 'RECTANGLE'
-        light_bot.blender_obj.data.size = 0.6
-        light_bot.blender_obj.data.size_y = 0.3
-        light_bot.set_location([0, -0.7, 1.4])
-        light_bot.set_rotation_euler([np.deg2rad(45), 0, 0])
-        light_bot.set_energy(10)
+        self.light_bot = bproc.types.Light('AREA', 'light_bot')
+        self.light_bot.blender_obj.data.shape = 'RECTANGLE'
+        self.light_bot.blender_obj.data.size = 0.6
+        self.light_bot.blender_obj.data.size_y = 0.3
+        self.light_bot.set_location([0, -0.7, 1.4])
+        self.light_bot.set_rotation_euler([np.deg2rad(45), 0, 0])
+        self.light_bot.set_energy(10)
