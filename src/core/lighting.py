4ce5d027cbfe615619eb70620d588648e5e9ec7a
okushnarev <styx.inc4@gmail.com>
2026-07-20 12:28:54 +0700

    Fix style

--- src/core/lighting.py
+++ src/core/lighting.py
@@ -1,8 +1,10 @@
 import blenderproc as bproc
 import numpy as np
 
+
 class BaseLighting:
     """Base class for lighting strategies."""
+
     def __init__(self, energy: int = 10, use_shadow: bool = True):
         self.energy = energy
         self.use_shadow = use_shadow
@@ -10,8 +12,10 @@ class BaseLighting:
     def setup(self) -> None:
         raise NotImplementedError('Each lighting strategy must implement the setup method.')
 
+
 class AmbientLighting(BaseLighting):
     """The dual area light setup with ambient sunlight and factory like lamps"""
+
     def setup(self) -> None:
         # Create ambient light
         self.sun = bproc.types.Light('SUN', 'ambient')
@@ -19,8 +23,10 @@ class AmbientLighting(BaseLighting):
         self.sun.blender_obj.data.angle = np.deg2rad(90)
         self.sun.set_energy(self.energy)
 
+
 class LocalFactoryLighting(BaseLighting):
     """The dual area light setup with factory like lamps"""
+
     def setup(self) -> None:
         # Create local lights
         self.light_top = bproc.types.Light('AREA', 'light_top')
@@ -31,7 +37,6 @@ class LocalFactoryLighting(BaseLighting):
         self.light_top.set_rotation_euler([np.deg2rad(-45), 0, 0])
         self.light_top.set_energy(self.energy)
         self.light_top.blender_obj.data.use_shadow = self.use_shadow
-
 
         self.light_bot = bproc.types.Light('AREA', 'light_bot')
         self.light_bot.blender_obj.data.shape = 'RECTANGLE'
